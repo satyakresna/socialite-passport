636b223f2cb6b5d8660cbcb8df2a6d560790f019
Satya Kresna <satyakresna6295@gmail.com>
2019-08-22 07:25:04 +0800

    Update auth api controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Socialite;
+use Route;
 
 class AuthController extends Controller
 {
-    public function redirectToProvider()
+    public function login(Request $request)
     {
-        return Socialite::driver('google')->redirect();
-    }
+        $form = [
+            'grant_type' => $request->grant_type,
+            'client_id' => config('services.passport.client_id'),
+            'client_secret' => config('services.passport.client_secret'),
+            'provider' => $request->provider,
+            'access_token' => $request->access_token,
+        ];
 
-    public function handleProviderCallback()
-    {
-        $user = Socialite::driver('google')->stateless()->user();
+        $request->request->add($form);
 
-        return response()->json($user);
+        $requestToken = Request::create('oauth/token', 'POST');
+        $response = Route::dispatch($requestToken);
+
+        return json_decode((string) $response->content(), true);
     }
 }
