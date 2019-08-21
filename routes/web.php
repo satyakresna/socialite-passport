a658c9846b1bd56a6a1483351a4705516cbee0bd
Satya Kresna <satyakresna6295@gmail.com>
2019-08-22 07:24:23 +0800

    Implement request user with socialite in LoginController and change web route

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login/google', 'Api\AuthController@redirectToProvider');
-Route::get('login/google/callback', 'Api\AuthController@handleProviderCallback');
+Route::get('login/google', 'Auth\LoginController@redirectToProvider');
+Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
